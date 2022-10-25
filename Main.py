4a402268284e2654f5fc2bc43684527e251bb7f3
URK21CS2047 <109648012+URK21CS2047@users.noreply.github.com>
2022-10-25 21:06:11 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 import re 
 def display_hash(hashtable) -> None: 
  for keyvalue in range(len(hashtable)): 
- cont = hashtable[keyvalue] 
- print(keyvalue, end = " ") 
+   cont = hashtable[keyvalue] 
+   print(keyvalue, end = " ") 
  for element in cont: 
- print("-->", element, end = " ") 
- print() 
+   print("-->", element, end = " ") 
+   print() 
 def Hashing(keyvalue) -> int: 
  return keyvalue % len(HashTable) 
 def insert(Hashtable, keyvalue, value) -> None: 
@@ -19,8 +19,8 @@ HashTable = [[] for _ in range(hash_table_size)]
 input_data = input() 
 data = [] 
 for item in re.split('], |].', input_data): 
- item = item[1:] 
- data = item.split(', ') 
+   item = item[1:] 
+   data = item.split(', ') 
  if len(data) > 1: 
- insert(HashTable, int(data[0]), data[1]) 
-display_hash (HashTable)
+   insert(HashTable, int(data[0]), data[1]) 
+   display_hash (HashTable)
